0b89a579d32a21e4c06ae4193f18b0b4f2dc25ae
Yogesh Nikam <yogeshnikam671@gmail.com>
2023-09-27 10:30:55 +0530

    Change formula description

--- Formula/btof.rb
+++ Formula/btof.rb
@@ -1,5 +1,5 @@
 class Btof < Formula
-  desc "A CLI tool to create a file with decoded content of a base64 string"
+  desc "CLI tool to create a file with decoded content of a base64 string"
   homepage "https://github.com/yogeshnikam671/base64-to-file"
   url "https://github.com/yogeshnikam671/base64-to-file/releases/download/v1.0.0/btof"
   sha256 "e3fac6cf0e9ce207f6c8afa3824c43c2093dfcf3c9cc7f1d1162074df8918ec7"
